386347b3117f36507e39c083a18e73effd60792c
Ruben Sousa <rubensousayt@gmail.com>
2022-01-03 20:42:49 +0000

    Inputs, Labels and Buttons

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -4,7 +4,7 @@ import { InitialState, UserReducer } from '../reducers/UserReducer';
 export const UserContext = createContext();
 
 export default ({children}) => {
-    const [state, dispactch] = useReducer(useReducer, InitialState);
+    const [state, dispatch] = useReducer(useReducer, InitialState);
 
     return(
         <UserContext.Provider value={{state,dispatch}}>
--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -1,11 +1,41 @@
 import React from "react";
-import { Text } from "react-native";
-import { Container } from './styles';
+import { 
+    Container,
+    InputArea,
+    CustomButton,
+    CustomButtonText,
+    SignMessageButton,
+    SignMessageButtonText,
+    SignMessageButtonTextBold
+ } from './styles';
+
+import BarberLogo from '../../assets/barber.svg';
+import EmailIcon from '../../assets/email.svg';
+import LockIcon from '../../assets/lock.svg'
+
+import SignInput from "../../components/SignInput";
+
 
 export default () => {
     return(
         <Container>
-            <Text>SignIn</Text>
+            <BarberLogo width="100%" height="160"/>
+
+            <InputArea>
+                <SignInput IconSvg={EmailIcon}/>
+                <SignInput IconSvg={LockIcon}/>
+
+            <CustomButton>
+                <CustomButtonText>Login</CustomButtonText>
+            </CustomButton>
+
+            </InputArea>
+
+            <SignMessageButton>
+                <SignMessageButtonText>Ainda não possui uma conta?</SignMessageButtonText>
+                <SignMessageButtonTextBold>Registe-se!</SignMessageButtonTextBold>
+            </SignMessageButton>
+
         </Container>
     );
 }
