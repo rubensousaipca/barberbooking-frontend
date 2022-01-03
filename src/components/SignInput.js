f6679903d5b4bfa62841853c7b28abd643775cde
Ruben Sousa <rubensousayt@gmail.com>
2022-01-03 20:59:33 +0000

    Editar Labels de Login

--- src/components/SignInput.js
+++ src/components/SignInput.js
@@ -1,5 +1,5 @@
-import react from "react";
-import styled from "styled-components/native";
+import React from 'react';
+import styled from 'styled-components/native';
 
 const InputArea = styled.View`
     width: 100%;
@@ -9,7 +9,7 @@ const InputArea = styled.View`
     border-radius: 30px;
     padding-left: 15px;
     align-items: center;
-    margin-bottom: 15 px;
+    margin-bottom: 15px;
 `;
 
 export default ({IconSvg}) => {
