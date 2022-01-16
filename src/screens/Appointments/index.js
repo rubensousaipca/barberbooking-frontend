b774613c75bac3dfb0f7f5555cfcd197427a9049
Ruben Sousa <rubensousayt@gmail.com>
2022-01-16 16:14:41 +0000

    Appointments Introduce

--- src/screens/Appointments/index.js
+++ src/screens/Appointments/index.js
@@ -1,11 +1,56 @@
-import React from 'react';
-import {Text} from 'react-native';
-import {Container} from './styles'
+import React, { useState, useEffect } from 'react';
+import { RefreshControl } from 'react-native';
+import {
+    Container,
+    Scroller,
+    ListArea,
+    EmptyWarning
+} from './styles';
+
+import AppointmentItem from '../../components/AppointmentItem';
+import Api from '../../Api';
 
 export default () => {
-    return(
+
+    const [loading, setLoading] = useState(false);
+    const [list, setList] = useState([]);
+
+    useEffect(()=>{
+        getAppointments();
+    }, []);
+
+    const getAppointments = async () => {
+        setLoading(true);
+        setList([]);
+
+        let res = await Api.getAppointments();
+        if(res.error == '') {
+            setList(res.list);
+        } else {
+            alert("Erro: "+res.error);
+        }
+
+        setLoading(false);
+    }
+
+    return (
         <Container>
-            <Text>Appointments</Text>
+
+            <Scroller refreshControl={
+                <RefreshControl refreshing={loading} onRefresh={getAppointments} />
+            }>
+
+                {!loading && list.length === 0 &&
+                    <EmptyWarning>Não há agendamentos.</EmptyWarning>
+                }
+
+                <ListArea>
+                    {list.map((item, k)=>(
+                        <AppointmentItem key={k} data={item} />
+                    ))}
+                </ListArea>
+            </Scroller>
+
         </Container>
     );
 }
